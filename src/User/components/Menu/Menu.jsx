a92f6f5b639418e2da11e6785c51106506efe95b
fullstackdevelopment <fullstackdevelopment@outlook.de>
2023-07-10 23:57:39 +0200

    HH-12 update-menu-style

--- src/User/components/Menu/Menu.jsx
+++ src/User/components/Menu/Menu.jsx
@@ -1,107 +1,80 @@
-import React, { useState } from 'react'
-import style from './style.module.css'
-import { Link } from 'react-router-dom'
-import { SidebarData } from '../Sidenav/SidebarData'
-import { icons } from '../Sidenav/SidebarData'
+import React, { useState } from 'react';
+import style from './style.module.css';
+import { Link } from 'react-router-dom';
+import { SidebarData, icons } from '../Sidenav/SidebarData';
 
 export const Menu = () => {
-    const [menuToggle, setMenuToggle] = useState(false)
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [toggleItem, setToggleItem] = useState(false);
+  const [changeColor, setChangeColor] = useState(false);
 
-    const [toggleItem, setToggleItem] = useState(false);
+  const changeColorHandle = () => {
+    setChangeColor(!changeColor);
+  };
 
-    const [changeColor, setChangeColor] = useState(false)
+  const toggleMenuHandle = (index) => {
+    index !== 1 ? setMenuToggle(!menuToggle) : setToggleItem(!toggleItem);
+  };
 
-    const changeColorHandel = () => {
-        setChangeColor(!changeColor)
-    };
-
-    const toggleMenuHandel = (index) => {
-        index !== 1 ? setMenuToggle(!menuToggle) : setToggleItem(!toggleItem)
-
-    }
-    return (
-        <div className={style.sidebarLayout}>
-            <ul className={style.menu}>
-                <Link variant="text" className={style.menuBtn} style={{ color: changeColor ? "white" : "black" }}
-                    onClick={() => { { toggleMenuHandel() } { changeColorHandel() } }}>Menu</Link>
-                <div className={style.controll}>
-                    {
-                        menuToggle && SidebarData.map((item, index) => {
-                            return (
-                                <>
-                                    <div key={item.id} className={style.row} >
-                                        <nav className={style.menuList}>
-                                            <span className={style.icons}>{item.icon}</span>
-                                            <Link onClick={() => { toggleMenuHandel(index) }}
-                                                to={item.link} className={style.title}>{item.title}</Link>
-                                        </nav>
-                                    </div>
-                                    {
-                                        toggleItem && item.item?.map((val) => {
-                                            return (
-                                                <div key={val.id} className={style.row} >
-                                                    <ul className={style.sidebarList}>
-                                                        <nav className={style.menuList}>
-                                                            <span className={style.icons}></span>
-                                                            <Link onClick={toggleMenuHandel} to={val.link} className={style.title}>{val.title}</Link>
-                                                        </nav>
-                                                    </ul>
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </>
-                            )
-                        })
-                    }
-                    <div className={style.media}>
-                        {
-                            menuToggle && icons.map((val) => {
-                                return (
-                                    <Link key={val.id} to={val.link} className={style.mediaList}>
-                                        {val.icon}
-                                    </Link>
-                                )
-                            })
-                        }
-                    </div>
-
+  return (
+    <div className={style.sidebarLayout}>
+      <ul className={style.menu}>
+        <Link
+          variant="text"
+          className={style.menuBtn}
+          style={{ color: changeColor && menuToggle ? 'white' : 'black' }}
+          onClick={() => {
+            toggleMenuHandle();
+            changeColorHandle();
+          }}
+        >
+          Menu
+        </Link>
+        <div className={style.controll}>
+          {menuToggle &&
+            SidebarData.map((item, index) => (
+              <>
+                <div key={item.id} className={style.row}>
+                  <nav className={style.menuList}>
+                    <span className={style.icons}>{item.icon}</span>
+                    <Link
+                      onClick={() => toggleMenuHandle(index)}
+                      to={item.link}
+                      className={style.title}
+                    >
+                      {item.title}
+                    </Link>
+                  </nav>
                 </div>
-
-            </ul>
-        </div>
-    )
-}
-
-/* import React, { useState } from 'react'
-import styleMobile from './styleMobile.module.css'
-import { Link } from 'react-router-dom'
-import Sidebar from '../Sidenav/Sidebar'
-
-export const Menu = () => {
-    const [menuToggle, setMenuToggle] = useState(false)
-    const [toggleItem, setToggleItem] = useState(false);
-    const [changeColor, setChangeColor] = useState(false)
-    const changeColorHandel = () => {
-        setChangeColor(!changeColor)
-    };
-    const toggleMenuHandel = (index) => {
-        index !== 1 ? setMenuToggle(!menuToggle) : setToggleItem(!toggleItem)
-    }
-    return (
-        <>
-            <div className={styleMobile.menuLayout}>
-                <ul className={styleMobile.menu}>
-                    <Link variant="text" className={styleMobile.menuBtn} styleMobile={{ color: changeColor ? "white" : "black" }}
-                        onClick={() => { { toggleMenuHandel() } { changeColorHandel() } }}>Menu</Link>
-                    <div className={styleMobile.controll}>
-                        {
-                            menuToggle && <Sidebar className={styleMobile.sidebar} />
-                        }
+                {toggleItem &&
+                  item.item?.map((val) => (
+                    <div key={val.id} className={style.row}>
+                      <ul className={style.sidebarList}>
+                        <nav className={style.menuList}>
+                          <span className={style.icons}></span>
+                          <Link
+                            onClick={toggleMenuHandle}
+                            to={val.link}
+                            className={style.title}
+                          >
+                            {val.title}
+                          </Link>
+                        </nav>
+                      </ul>
                     </div>
-
-                </ul>
-            </div>
-        </>
-    )
-} */
+                  ))}
+              </>
+            ))}
+          <div className={style.media}>
+            {menuToggle &&
+              icons.map((val) => (
+                <Link key={val.id} to={val.link} className={style.mediaList}>
+                  {val.icon}
+                </Link>
+              ))}
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+};
