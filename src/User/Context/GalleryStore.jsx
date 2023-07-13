8d8eec9e4fc9d9a7b7dd468c16229d1377e558dd
fullstackdevelopment <fullstackdevelopment@outlook.de>
2023-07-14 01:45:52 +0200

    HH-18 implement-portrait-gallery

--- src/User/Context/GalleryStore.jsx
+++ src/User/Context/GalleryStore.jsx
@@ -7,11 +7,14 @@ import React from 'react'
 export const GalleryStore = ({children}) => {
   const [streetGallery, setStreetGallery]= useState([])
   const [landscapeGallery, setLandscapetGallery]= useState([])
+  const [portraitGallery, setPortraitGallery]= useState([])
   const [project, setProject]= useState([])
 
   const landscapeImageUrl = "http://localhost:9000/landscapeImage"
   const streetImageUrl = "http://localhost:9000/streetImage"
   const projectUrl = "http://localhost:9000/projectCard"
+  const portraitImageUrl = "http://localhost:9000/portraitImage"
+
 
   useEffect(() => {
     axios.get(landscapeImageUrl).then((res) => {
@@ -28,9 +31,14 @@ export const GalleryStore = ({children}) => {
         setProject(res.data)
     })
   }, [])
+  useEffect(() => {
+    axios.get(portraitImageUrl).then((res) => {
+      setPortraitGallery(res.data)
+    })
+  }, [])
     return (
     <GalleryContext.Provider value={{
-        streetGallery, landscapeGallery, project
+        streetGallery, landscapeGallery, project, portraitGallery
     }}>
         {
             children 
