d9bc462cd01000546dbbeb2994a03b8c4c3e889e
fullstackdevelopment <fullstackdevelopment@outlook.de>
2023-07-14 02:09:44 +0200

    HH-26 create-hover-image-portrait-page

--- src/User/pages/Photography/Portrait/Portrait.jsx
+++ src/User/pages/Photography/Portrait/Portrait.jsx
@@ -8,6 +8,19 @@ import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
 const Portrait = () => {
   const { portraitGallery } = useContext(GalleryContext);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (index) => {
+    setSelectedImage(portraitGallery[index]);
+  };
+
+  const handleImageHover = (index) => {
+    setSelectedImage(portraitGallery[index]);
+  };
+
+  const handleImageLeave = () => {
+    setSelectedImage(null);
+  };
 
   return (
     <div className={style.portrait}>
@@ -15,13 +28,33 @@ const Portrait = () => {
         <h1 className={style.header}>Landscape Gallery</h1>
         {portraitGallery.map((item, index) => (
           <div key={item.id} className={style.gallery}>
-            <div className={style.single}>
-              <Link to={`/portrait/portraitImage/${index}`}>
+            <div
+              className={style.single}
+              onMouseEnter={() => handleImageHover(index)}
+              onMouseLeave={handleImageLeave}
+            >
+              <Link to={`/portrait/portraitImage/${index}`} onClick={() => handleImageClick(index)}>
                 <img src={item.original} alt='gallery' className={style.img} />
+                {selectedImage === item && (
+                  <div className={style.imageOverlay}>
+                    <div className={style.imageTitle}>{item.title}</div>
+                    <div className={style.buttonGroup}>
+                      <IconButton className={style.likeButton}>
+                        <FavoriteIcon style={{ color: '#FFFFFF', fontSize: '28px' }} />
+                      </IconButton>
+
+                      <IconButton className={style.likeButton}>
+                        <ThumbDownIcon style={{ color: '#FFFFFF', fontSize: '28px' }} />
+                      </IconButton>
+                    </div>
+                    {/* <div className={style.buttonGroup}>
+                    <button className={style.likeButton2} id={style.likeButton2}>Like</button>
+                    <button className={style.likeButton2}>Share</button>
+                    </div> */}
+                  </div>
+                )}
               </Link>
             </div>
-
-         
           </div>
         ))}
       </div>
@@ -30,3 +63,4 @@ const Portrait = () => {
 };
 
 export default memo(Portrait);
+
