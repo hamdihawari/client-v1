e635055d2a6aad5fc8149b3307eae3fd8eb283fc
hamdihawari <hamdi.hawari@outlook.com>
2023-08-13 04:39:38 +0200

    HH-34 create-menu-toggle

--- src/User/pages/Project/ProjectItem/ProjectItem.jsx
+++ src/User/pages/Project/ProjectItem/ProjectItem.jsx
@@ -1,14 +1,11 @@
 import style from './style.module.css';
 import { Link } from 'react-router-dom';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import SendIcon from '@mui/icons-material/Send';
 import React, { useState } from 'react';
-
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShareIcon from '@mui/icons-material/Share';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { IconButton } from '@mui/material';
-
+import { MenuToggle } from '../../../components/MenuToggle/MenuToggle';
+import menuToggleStyles from './style.module.css';
 
 export const ProjectItem = ({ id, title, image, description, imageHover, path }) => {
 
@@ -44,9 +41,9 @@ export const ProjectItem = ({ id, title, image, description, imageHover, path })
         <IconButton className={style.icon} id={style.SendIcon}>
           <ShareIcon style={{ color: '#000000', fontSize: '26px' }}/>
         </IconButton>
-        <IconButton className={style.icon} id={style.moreHorizIcon}>
-          <MoreHorizIcon style={{ color: '#000000', fontSize: '26px' }}/>
-        </IconButton>
+
+        <MenuToggle/>
+        
       </div>
       </div>
     </div>
