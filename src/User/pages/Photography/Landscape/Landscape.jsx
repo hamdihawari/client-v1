260254b53a8d0b8791ad8ee3938e259225ef8454
hamdihawari <hamdi.hawari@outlook.com>
2023-09-01 01:36:17 +0200

    HH-54 create-paginat-for-landscape-page

--- src/User/pages/Photography/Landscape/Landscape.jsx
+++ src/User/pages/Photography/Landscape/Landscape.jsx
@@ -9,7 +9,7 @@ import SearchBar from '../../../components/SearchBar/SearchBar';
 import useMediaQuery from '../../../Hooks/useMediaQuery';
 import {ScrollToTopButton} from '../../../components/ScrollToTopButton/ScrollToTopButton'
 import rtlStyle from './rtl.module.css'
-
+import { Paginate } from '../../../components/Paginate/Paginate';
 
 const Landscape = () => {
   const { landscapeGallery, isArabic } = useContext(GalleryContext);
@@ -17,6 +17,21 @@ const Landscape = () => {
   const [selectedCity, setSelectedCity] = useState('');
   const isLargeMobile = useMediaQuery('(min-width:992px)'); // Laptop 
   const [searchTerm, setSearchTerm] = useState('');
+
+  // Pagination setup
+  const itemsPerPage = 9 // Adjust this based on your requirements
+  const totalItems = landscapeGallery.length // Total number of items you're paginating
+  const pageCount = Math.ceil(totalItems / itemsPerPage) /* calculates the total number of pages needed for pagination based on the total number of items and the number of items to display per page. */
+  const [currentPage, setCurrentPage] = useState(0)
+  // Calculate the start and end indexes of items for the current page
+  const startIndex = currentPage * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+  // Get the projects for the current page
+  const streetGalleryToShow = landscapeGallery.slice(startIndex, endIndex) /* used to create a subset of the original list of projects that should be displayed on the current page of your pagination. */
+
 
   const handleImageClick = (index) => {
     setSelectedImage(landscapeGallery[index]);
@@ -44,7 +59,7 @@ const Landscape = () => {
   return (
     <div className={style.landscape}>
         <div className={`${style.landscapeHeader} ${isArabic && rtlStyle.landscapeHeader}`}>
-        {landscapeGallery.map(val =>{
+        {streetGalleryToShow.map(val =>{
           return<h1 key={val.id} className={`${style.header} ${isArabic && rtlStyle.header}`}>{val.headerTitle} {val.smile}</h1>
         })}
         {isLargeMobile && <SearchBar onSearchBarChange={handleSearchInputChange} /* customStyle={customHeaderStyle} */ onCityChange={setSelectedCity} />}
@@ -79,6 +94,7 @@ const Landscape = () => {
         ))}
       </div>
       <ScrollToTopButton />
+      <Paginate pageCount={pageCount} onPageChange={handlePageChange} itemsPerPage={8}/>
     </div>
   );
 };
