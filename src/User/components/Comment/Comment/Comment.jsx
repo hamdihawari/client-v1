e1fa86ae5bca0b6706bca9e568bfdeebc2f0d89e
hamdihawari <hamdi.hawari@outlook.com>
2023-09-12 03:31:15 +0200

    HH-56 add-reply-comment

--- src/User/components/Comment/Comment/Comment.jsx
+++ src/User/components/Comment/Comment/Comment.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './style.module.css'
 import userperson from '../../../../assets/images/girlUser.png'
+import { CommentForm } from '../CommentForm/CommentForm'
+import { AddComment } from '@mui/icons-material'
 
-export const Comment = ({ comment, replies }) => {
+export const Comment = ({ comment, replies, currentUserId, activeComment, setActiveComment, addComment, parentId=null }) => {
+  
+  const canReply = Boolean(currentUserId) // Reply
+  const createdAt = new Date(comment.createdAt).toLocaleDateString()
+  const isReplying = activeComment && activeComment.type === 'replying' && activeComment.id === comment.id
+  const replyId = parentId ? parentId : comment.id
   return (
     <>
       <div className={style.comment}>
@@ -12,19 +19,40 @@ export const Comment = ({ comment, replies }) => {
         <div className={style.commentRightPart}>
           <div className={style.commentsContent}>
             <p className={style.commentAuthor}>{comment.username}</p>
-            <p className={style.commentcreatedAt}>{comment.createdAt}</p>
+            <p className={style.commentcreatedAt}>{createdAt}</p>
           </div>
           <div className={style.commentText}>{comment.body}</div>
+          <div className={style.commentActions}>
+          {canReply && (
+          <div className={style.commentActions}
+          onClick={() => 
+            setActiveComment({id: comment.id, type: "replying"})}>
+              Reply
+              </div>
+              )}
+          </div>
+          {isReplying && (
+            <CommentForm 
+            submitLabel="Reply"
+            handleSubmit={(text) => 
+              addComment(text, replyId)}/>
+          )}
           {replies.length > 0 && (
             <div className={style.replies}>
               {replies.map(reply => (
-                <Comment comment={reply} key={reply.id} replies={[]} />
+                <Comment 
+                comment={reply}
+                key={reply.id}
+                replies={[]}
+                currentUserId={currentUserId}
+                parentId={comment.id}
+                addComment={addComment}
+                />
               ))}
             </div>
           )}
         </div>
       </div>
     </>
-
   )
 }
--- src/User/components/Comment/CommentForm/CommentForm.jsx
+++ src/User/components/Comment/CommentForm/CommentForm.jsx
@@ -5,37 +5,24 @@ import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import { Button } from '@mui/material'
 import rtlStyle from './rtl.module.css'
 
-export const CommentForm = ({ handleSubmit, imageID }) => {
+export const CommentForm = ({ handleSubmit, submitLabel }) => {
   const [text, setText] = useState('');
   const [isInputFocused, setInputFocused] = useState(false);
 
   const handleOnSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (text.trim() === '') {
-      return
+      return;
     }
-    try {
-      const response = await axios.post('http://localhost:9000/getComments', {
-        body: text,
-        userId: '1',
-        imageID: imageID,
-        parentId: null,
-        username: 'Johanna',
-        createdAt: new Date().toISOString(),
-      });
-      console.log('Created comment:', response.data)
-      setText('');
-      handleSubmit(response.data);
-    } catch (error) {
-      console.error('Error creating comment:', error)
-    }
-  };
+    handleSubmit(text, null); 
+    setText('');
+  }
 
   const handleInputChange = (e) => {
     setText(e.target.value)
   };
 
-    const handleInputFocus = () => {
+  const handleInputFocus = () => {
     setInputFocused(true)
   };
 
@@ -56,7 +43,7 @@ export const CommentForm = ({ handleSubmit, imageID }) => {
       />
       {isInputFocused && (
         <div className={style.buttonContainer}>
-           <Button variant="text" onClick={handleCancelButtonClick} className={style.cancelButton}>
+          <Button variant="text" onClick={handleCancelButtonClick} className={style.cancelButton}>
             Cancel
           </Button>
           <Button variant="contained" onClick={handleOnSubmit} className={style.enterButton}>
@@ -68,3 +55,25 @@ export const CommentForm = ({ handleSubmit, imageID }) => {
     </form>
   )
 }
+
+
+
+/* if (text.trim() === '') {
+  return
+}
+try {
+  const response = await axios.post('http://localhost:9000/comment', {
+    body: text,
+    userId: '1',
+    imageID: imageID,
+    parentId: null,
+    username: 'Johanna',
+    createdAt: new Date().toISOString(),
+  });
+  console.log('Created comment:', response.data)
+  setText('');
+  handleSubmit(response.data);
+} catch (error) {
+  console.error('Error creating comment:', error)
+}
+}; */
