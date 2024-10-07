fe18112ed13b705b700708ab21f66735e172e6ea
hamdihawari <hamdi.hawari@outlook.com>
2024-10-07 17:24:36 +0200

    HH-79 <create AboutProvidor with multilanguages, about_me and about_me_translation>

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './index.css';
 import './i18n.js';
 import { GalleryStore } from './User/Context/GalleryStore.jsx';
 import { ProjectProvider } from './User/Context/ProjectProvidor.jsx';
+import AboutProvidor from "./User/Context/AboutProvidor.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <GalleryStore>
             <ProjectProvider>
+                <AboutProvidor>
                 <App />
+                </AboutProvidor>
             </ProjectProvider>
         </GalleryStore>
     </React.StrictMode>
